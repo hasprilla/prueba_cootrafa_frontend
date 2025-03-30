2ecd625e46e1acea53992e8417b52b5d421401cf
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-03-30 18:42:16 -0500

    Url remota

--- lib/core/api/api_url.dart
+++ lib/core/api/api_url.dart
@@ -1,8 +1,7 @@
 class ApiUrl {
   const ApiUrl._();
 
-  // static const baseUrl = 'https://pruebacootrafabackend.haaspes.space/api';
-  static const baseUrl = 'http://127.0.0.1:8000/api';
+  static const baseUrl = 'https://pruebacootrafabackend.haaspes.space/api';
   static const products = '/products';
   static const inventary = '/inventary';
 }
