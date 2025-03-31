815967b193f7fae898c1c12c6414de97903612cd
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-03-30 22:44:03 -0500

    Validaciones productos

--- lib/features/product/presentation/widgets/show_product_dialog.dart
+++ lib/features/product/presentation/widgets/show_product_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/product_bloc.dart';
@@ -6,8 +7,9 @@ import '../bloc/product_bloc.dart';
 void showProductDialog({
   required BuildContext context,
   Map<String, dynamic>? productJson,
-  required List<Map<String, dynamic>> inventoryOptions, // Add this parameter
+  required List<Map<String, dynamic>> inventoryOptions,
 }) {
+  final _formKey = GlobalKey<FormState>();
   final idController = TextEditingController(text: productJson?['id'] ?? '');
   final nameController = TextEditingController(
     text: productJson?['name'] ?? '',
@@ -19,10 +21,81 @@ void showProductDialog({
     text: productJson?['price']?.toString() ?? '',
   );
   final quantityController = TextEditingController(
-    text: productJson?['quantity']?.toString() ?? '',
+    text: productJson?['quantity']?.toString() ?? '1',
   );
 
   String? selectedInventoryId = productJson?['inventoryId']?.toString();
+
+  String? validateName(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Ingrese un nombre válido';
+    }
+    if (!RegExp(r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$').hasMatch(value)) {
+      return 'Solo se permiten letras y espacios';
+    }
+    return null;
+  }
+
+  String? validateBarcode(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Ingrese un código de barras';
+    }
+    return null;
+  }
+
+  String? validatePrice(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Ingrese un precio';
+    }
+    final price = double.tryParse(value);
+    if (price == null || price <= 0) {
+      return 'Debe ser mayor a 0';
+    }
+    return null;
+  }
+
+  String? validateQuantity(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Ingrese una cantidad';
+    }
+    final quantity = int.tryParse(value);
+    if (quantity == null || quantity < 0) {
+      return 'Debe ser 0 o mayor';
+    }
+    return null;
+  }
+
+  String? validateInventory(String? value) {
+    if (value == null) {
+      return 'Seleccione un inventario';
+    }
+    return null;
+  }
+
+  void submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final productBloc = context.read<ProductBloc>();
+      final event =
+          productJson?['id'] == null
+              ? CreateProductEvent(
+                inventoryId: int.parse(selectedInventoryId!),
+                name: nameController.text.trim(),
+                barcode: barcodeController.text.trim(),
+                price: priceController.text,
+                quantity: int.parse(quantityController.text),
+              )
+              : UpdateProductEvent(
+                id: int.parse(idController.text),
+                inventoryId: int.parse(selectedInventoryId!),
+                name: nameController.text.trim(),
+                barcode: barcodeController.text.trim(),
+                price: priceController.text,
+                quantity: int.parse(quantityController.text),
+              );
+
+      productBloc.add(event);
+    }
+  }
 
   showDialog(
     context: context,
@@ -33,165 +106,137 @@ void showProductDialog({
           borderRadius: BorderRadius.circular(12.0),
         ),
         elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  productJson?['id'] == null
-                      ? 'Crear Producto'
-                      : 'Editar Producto',
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: idController,
-                  decoration: InputDecoration(
-                    labelText: 'ID',
-                    border: OutlineInputBorder(),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                  ),
-
-                  enabled: false,
-                ),
-                const SizedBox(height: 12),
-                DropdownButtonFormField<String>(
-                  value: selectedInventoryId,
-                  decoration: InputDecoration(
-                    labelText: 'Inventorio',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.inventory),
-                  ),
-                  items:
-                      inventoryOptions.map((inventory) {
-                        return DropdownMenuItem<String>(
-                          value: inventory['id'].toString(),
-                          child: Text(
-                            inventory['value'] ?? inventory['id'].toString(),
-                          ),
-                        );
-                      }).toList(),
-                  onChanged: (String? newValue) {
-                    selectedInventoryId = newValue;
-                  },
-                ),
-                const SizedBox(height: 12),
-                TextField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: 'Nombre del Producto',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.shopping_bag),
-                  ),
-                  textCapitalization: TextCapitalization.words,
-                ),
-                const SizedBox(height: 12),
-                TextField(
-                  controller: barcodeController,
-                  decoration: InputDecoration(
-                    labelText: 'Código de barras',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.barcode_reader),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 12),
-                TextField(
-                  controller: priceController,
-                  decoration: InputDecoration(
-                    labelText: 'Precio',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.attach_money),
-                    suffixText: 'USD',
-                  ),
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-                ),
-                const SizedBox(height: 12),
-                TextField(
-                  controller: quantityController,
-                  decoration: InputDecoration(
-                    labelText: 'Cantidad en Stock',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.format_list_numbered),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 12,
-                        ),
-                      ),
-                      child: Text(
-                        'CANCELAR',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    productJson?['id'] == null
+                        ? 'Crear Producto'
+                        : 'Editar Producto',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
                     ),
-                    const SizedBox(width: 8),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (selectedInventoryId == null) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Seleccione un Inventory ID'),
-                            ),
-                          );
-                          return;
-                        }
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
 
-                        if (productJson?['id'] == null) {
-                          context.read<ProductBloc>().add(
-                            CreateProductEvent(
-                              inventoryId: int.parse(
-                                selectedInventoryId ?? '0',
-                              ),
-                              name: nameController.text,
-                              barcode: barcodeController.text,
-                              price: priceController.text,
-                              quantity:
-                                  int.tryParse(quantityController.text) ?? 0,
-                            ),
-                          );
-                        } else {
-                          context.read<ProductBloc>().add(
-                            UpdateProductEvent(
-                              id: int.tryParse(idController.text) ?? 0,
-                              inventoryId: int.parse(selectedInventoryId!),
-                              name: nameController.text,
-                              barcode: barcodeController.text,
-                              price: priceController.text,
-                              quantity:
-                                  int.tryParse(quantityController.text) ?? 0,
-                            ),
-                          );
-                        }
-                        Navigator.of(context).pop();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 12,
-                        ),
-                      ),
-                      child: Text('GUARDAR'),
+                  TextFormField(
+                    controller: idController,
+                    decoration: const InputDecoration(
+                      labelText: 'ID',
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.grey,
                     ),
-                  ],
-                ),
-              ],
+                    enabled: false,
+                  ),
+                  const SizedBox(height: 12),
+
+                  DropdownButtonFormField<String>(
+                    value: selectedInventoryId,
+                    decoration: InputDecoration(
+                      labelText: 'Inventario',
+                      border: const OutlineInputBorder(),
+                      errorStyle: TextStyle(color: Colors.red),
+                    ),
+                    items:
+                        inventoryOptions.map((inventory) {
+                          return DropdownMenuItem<String>(
+                            value: inventory['id'].toString(),
+                            child: Text(
+                              inventory['value'] ?? inventory['id'].toString(),
+                            ),
+                          );
+                        }).toList(),
+                    onChanged: (String? newValue) {
+                      selectedInventoryId = newValue;
+                    },
+                    validator: validateInventory,
+                  ),
+                  const SizedBox(height: 12),
+
+                  TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Nombre del Producto',
+                      border: OutlineInputBorder(),
+                    ),
+                    textCapitalization: TextCapitalization.words,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]'),
+                      ),
+                    ],
+                    validator: validateName,
+                  ),
+                  const SizedBox(height: 12),
+
+                  TextFormField(
+                    controller: barcodeController,
+                    decoration: const InputDecoration(
+                      labelText: 'Código de barras',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: validateBarcode,
+                  ),
+                  const SizedBox(height: 12),
+
+                  TextFormField(
+                    controller: priceController,
+                    decoration: const InputDecoration(
+                      labelText: 'Precio',
+                      border: OutlineInputBorder(),
+                      suffixText: 'USD',
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: true,
+                    ),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'^\d*\.?\d{0,2}'),
+                      ),
+                    ],
+                    validator: validatePrice,
+                  ),
+                  const SizedBox(height: 12),
+
+                  TextFormField(
+                    controller: quantityController,
+                    decoration: const InputDecoration(
+                      labelText: 'Cantidad en Stock',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: validateQuantity,
+                  ),
+                  const SizedBox(height: 24),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text('CANCELAR'),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: submitForm,
+                        child: const Text('GUARDAR'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
